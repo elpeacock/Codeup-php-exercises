2d6ea92fde75c310128abe2855f5ac74d7f2a821
Liz Peacock <liz.peacock11@gmail.com>
2016-08-31 14:58:03 -0500

    committing completed foreach exercise

--- foreach.php
+++ foreach.php
@@ -3,7 +3,36 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-    if (is_scalar($thing)) {
-        echo "{$thing} is scalar" . PHP_EOL;
+    if (is_array($thing)) {
+        echo "type is array" . PHP_EOL;
+    } else if (is_bool($thing)) {
+        echo "type is boolean" . PHP_EOL;
+    } else if (is_integer($thing)) {
+        echo "type is integer" . PHP_EOL;
+    } else if (is_float($thing)) {
+        echo "type is float" . PHP_EOL;
+    } else if (is_null($thing)) {
+        echo "type is null" . PHP_EOL;
+    } else if (is_string($thing)) {
+        echo "type is string" . PHP_EOL;
     }
+};
+
+foreach ($things as $thing) {
+    if (is_scalar($thing)) {
+        echo "{$thing} is scalar, type is " . gettype($thing) . PHP_EOL;
+    }
+};
+
+foreach ($things as $thing) {
+    $message = "the type is " . gettype($thing) . ". The value is: ";
+    if (is_array($thing)) {
+        foreach ($thing as $piece) {
+            $message .= "{$piece}" . " ";
+        }
+    } else {
+        $message .= "{$thing}";
+    }
+    $message .= PHP_EOL;
+    echo $message;
 }
