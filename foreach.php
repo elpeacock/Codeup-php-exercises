cab5d92b1a23ad474ed51eaf8d3b87c11d67c431
Liz Peacock <liz.peacock11@gmail.com>
2016-08-31 12:26:53 -0500

    adding part 2 of foreach exercises

--- foreach.php
+++ foreach.php
@@ -3,17 +3,7 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-    if (is_array($thing)) {
-        echo "{$thing} is an array" . PHP_EOL;
-    } else if (is_bool($thing)) {
-        echo "{$thing} is a boolean" . PHP_EOL;
-    } else if (is_string($thing)) {
-        echo "{$thing} is a string" . PHP_EOL;
-    } else if (is_null($thing)) {
-        echo "{$thing} is value type null" . PHP_EOL;
-    } else if (is_integer($thing)) {
-        echo "{$thing} is an integer" . PHP_EOL;
-    } else if (is_float($thing)) {
-        echo "{$thing} is a float" . PHP_EOL;
+    if (is_scalar($thing)) {
+        echo "{$thing} is scalar" . PHP_EOL;
     }
 }
