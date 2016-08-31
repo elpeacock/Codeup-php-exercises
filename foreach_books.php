d3dcbfe5d04c0c6edf3d6692dbec29e56a005d9f
Liz Peacock <liz.peacock11@gmail.com>
2016-08-31 15:56:43 -0500

    committing part one of books exercise

---
+++ foreach_books.php
@@ -0,0 +1,35 @@
+<?php
+
+$books = [
+    'The Hobbit' => [
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ],
+    'Game of Thrones' => [
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ],
+    'The Catcher in the Rye' => [
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ],
+    'A Tale of Two Cities' => [
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    ]
+];
+
+$message = "";
+
+foreach ($books as $book => $bookInfo) {
+    $message .= "{$book}:" . PHP_EOL;
+    foreach($bookInfo as $key => $value){
+        $message .= "   {$key}: {$value}" . PHP_EOL;
+    }
+};
+
+echo $message;
