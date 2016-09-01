599dc1aec9d92e6f167043a02b0b15a86462ea59
Liz Peacock <liz.peacock11@gmail.com>
2016-09-01 11:47:41 -0500

    adding modulus functionality to arithmetic.

--- arithmetic.php
+++ arithmetic.php
@@ -16,8 +16,12 @@ function divide($a, $b) {
     return $a / $b;
 }
 
+function modulus($a, $b) {
+	return $a % $b;
+}
 // Add code to test your functions here
 echo add(1, 5) . PHP_EOL;
 echo subtract(6, 4) . PHP_EOL;
 echo multiply(3, 5) . PHP_EOL;
 echo divide(4, 2) . PHP_EOL;
+echo modulus(5, 3) . PHP_EOL;
