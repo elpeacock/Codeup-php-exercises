bf7cbf63f5883aef08279aa206c2ceda7addb6f4
Liz Peacock <liz.peacock11@gmail.com>
2016-09-01 11:45:22 -0500

    adding arithmetic step 1.

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,23 @@
 <?php
 
-function add($a, $b)
-{
+function add($a, $b) {
     return $a + $b;
 }
 
-function subtract($a, $b)
-{
-    // Add code here
+function subtract($a, $b) {
+    return $a - $b;
 }
 
-function multiply($a, $b)
-{
-    // Add code here
+function multiply($a, $b) {
+    return $a * $b;
 }
 
-function divide($a, $b)
-{
-    // Add code here
+function divide($a, $b) {
+    return $a / $b;
 }
 
 // Add code to test your functions here
+echo add(1, 5) . PHP_EOL;
+echo subtract(6, 4) . PHP_EOL;
+echo multiply(3, 5) . PHP_EOL;
+echo divide(4, 2) . PHP_EOL;
