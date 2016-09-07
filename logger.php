1578c2df3350054ae6d607be476c26b7e3874415
Liz Peacock <liz.peacock11@gmail.com>
2016-09-07 18:43:57 -0500

    exercises to practice writing to files in php

---
+++ logger.php
@@ -0,0 +1,25 @@
+<?php
+
+function logMessage($logLevel, $message)
+{
+    // todo - complete this function
+    // log file name needs to change according to date YYYY-MM-DD.log
+	$today = date("Y-m-d");
+	$timestamp = date("H:i:s e");
+	$fileToWrite = "log-{$today}.log";
+	$output = "$today" . " $timestamp " . "$logLevel" . ": " . "$message" . PHP_EOL
+    $handle = fopen($fileToWrite, 'a');
+    fwrite ($handle, $output);
+    fclose($handle);
+}
+
+function logInfo($message) {
+	logMessage('INFO', $message);
+}
+
+function logError($message) {
+	logMessage('ERROR', $message);
+}
+
+logInfo("You did something and it worked.");
+logError("Nope, you fucked it up.");
