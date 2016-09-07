b10cf12086cf37f046d904070b098135e281ba3e
Liz Peacock <liz.peacock11@gmail.com>
2016-09-07 18:42:34 -0500

    trying to sort by a specific key in the array. still need to figure this out

--- sales-report.php
+++ sales-report.php
@@ -26,21 +26,34 @@ function importSalesReport($filename)
     $monthlySales = [];
 
     $unitsSold = '';
-    
-    foreach ($salesReport as $key => $seller) {
-    	$seller = explode(', ', $seller);
-    	$person = [];
-    	$person['employee number'] = $seller[0];
-    	$person['first name'] = $seller[1];
-    	$person['last name'] = $seller[2];
-    	$person['units sold'] = $seller[3];
-    	array_push($monthlySales, $person);
-    	$unitsSold += ($person['units sold']);
-    	return($unitsSold);
 
+    foreach ($salesReport as $key => $seller) {
+        $seller = explode(', ', $seller);
+        $person = [];
+        $person['employee number'] = $seller[0];
+        $person['first name'] = $seller[1];
+        $person['last name'] = $seller[2];
+        $person['units sold'] = $seller[3];
+        array_push($monthlySales, $person);
+        $unitsSold += $person['units sold'];
     }
+
     $numberOfEmployees = (count($monthlySales));
-    echo "Total number of employees: $numberOfEmployees" . PHP_EOL;
-    echo "Total units sold this month: $unitsSold" . PHP_EOL;
+    $averageUnits = $unitsSold/$numberOfEmployees;
+    $results = <<<PRINTME
+=================================Monthly Sales Data===============================
+    Total number of employees: {$numberOfEmployees}.
+    Total units sold this month: {$unitsSold}.
+    Average Units sold per employee: {$averageUnits}.
+============================Monthly Employee Production===========================
+PRINTME;
+    
+    foreach ($monthlySales as $person) {
+        foreach ($person['units sold'] as $value) {
+            var_dump($value);
+        }
+    }
+    echo "$results" . PHP_EOL;
+
 }
 importSalesReport($data);
