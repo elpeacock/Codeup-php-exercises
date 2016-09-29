d934ad062d70304caca4176d588f75ea7fa1af91
Liz Peacock <liz.peacock11@gmail.com>
2016-09-29 11:22:28 -0500

    updated Model class to practice late static methods added User class to practice late static methods added model user test to run late static methods

--- Model.php
+++ Model.php
@@ -2,30 +2,40 @@
 
 class Model
 {
-	protected $attributes = [];
+	private $attributes = [];
+	protected static $table = 'model';
 
 	//magically put stuffs in attributes (protected)
 	public function __set($name, $value)
 	{
 		$this->attributes[$name] = $value;
-		var_dump($this->attributes);
+		
 	}
 
 	//magically get stuffs from attributes (protected)
 	public function __get($name)
 	{
 		if (isset($this->attributes[$name])) {
-			echo $this->attributes[$name] . PHP_EOL;
+			return $this->attributes[$name];
 		} 
 		return null;
 		
 	}
+
+	//add static method to return table
+	public static function getTableName()
+	{
+		//called from users, keyword self:: returns model
+		// return self::$table;
+		//called from users, keyword static:: returns users
+		return static::$table;
+	}
 }
 
-$thing = new Model();
+// $thing = new Model();
 
-$thing->color = 'yellow';
-$thing->type = 'backpack';
-$thing->color;
-$thing->type;
-$thing->purse;
+// $thing->color = 'yellow';
+// $thing->type = 'backpack';
+// $thing->color;
+// $thing->type;
+// $thing->purse;
