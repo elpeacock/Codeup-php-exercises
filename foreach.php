0ac64b2108f1ab5d936543e8160320781c2fae37
Liz Peacock <liz.peacock11@gmail.com>
2017-03-15 19:24:57 -0500

    refactored this a bit

--- foreach.php
+++ foreach.php
@@ -3,19 +3,20 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-    if (is_array($thing)) {
-        echo "type is array" . PHP_EOL;
-    } else if (is_bool($thing)) {
-        echo "type is boolean" . PHP_EOL;
-    } else if (is_integer($thing)) {
-        echo "type is integer" . PHP_EOL;
-    } else if (is_float($thing)) {
-        echo "type is float" . PHP_EOL;
-    } else if (is_null($thing)) {
-        echo "type is null" . PHP_EOL;
-    } else if (is_string($thing)) {
-        echo "type is string" . PHP_EOL;
-    }
+    // if (is_array($thing)) {
+    //     echo "type is array" . PHP_EOL;
+    // } else if (is_bool($thing)) {
+    //     echo "type is boolean" . PHP_EOL;
+    // } else if (is_integer($thing)) {
+    //     echo "type is integer" . PHP_EOL;
+    // } else if (is_float($thing)) {
+    //     echo "type is float" . PHP_EOL;
+    // } else if (is_null($thing)) {
+    //     echo "type is null" . PHP_EOL;
+    // } else if (is_string($thing)) {
+    //     echo "type is string" . PHP_EOL;
+    // }
+    echo "type is: " . gettype($thing) . PHP_EOL;
 };
 
 foreach ($things as $thing) {
