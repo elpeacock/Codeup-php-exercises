32c0587f037591183aba0b71c2d63965e9af08e8
Liz Peacock <liz.peacock11@gmail.com>
2017-03-23 17:39:26 -0500

    slightly refactored and used some code in another file

--- contact-parser.php
+++ contact-parser.php
@@ -1,4 +1,5 @@
 <?php
+// todo - read file and parse contacts
 
 $data = $argv[1];
 
@@ -15,27 +16,38 @@ function parseContacts($filename)
 
     fclose($handle);
     
+    // explode the text file on a new line to get an array of people
     $contacts = explode(PHP_EOL, $contacts);
 
+    // take off the extra new line
     array_pop($contacts);
 
+    // loop thru the array
     foreach ($contacts as $key => $contact) {
+        // explode each contact on the pipe
         $contact = explode('|', $contact);
+        // initialize an array to save each contact as a new array
         $contactArray = [];
+        // reassign the numerical index to a named key
         $contactArray ['name'] = $contact[0];
         $contactArray ['number'] = $contact[1];
+        // store the new formatted array as an individual contact 
         $contact = $contactArray;
+        // format the phone number
         $value = substr_replace(($contact['number']), '-', 3, 0);
         $value2 = substr_replace($value, '-', 7, 0);
+        // save the formatted phone number back to $contact[number]
         $contact['number'] = $value2;
+        // push the new array we created for each contact back onto the original array of contacts
         array_push($contacts, $contact);
         
     }
+
+    // remove the initial unformatted information from the array of contacts
     array_shift($contacts);
     array_shift($contacts);
     array_shift($contacts);
 
-    // todo - read file and parse contacts
 
     return $contacts;
 
